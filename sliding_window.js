765d1a45a80f85565a15bd2765599ba5765eb65b
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2022-06-04 22:36:20 +0200

    sliding window pattern

---
+++ sliding_window.js
@@ -0,0 +1,55 @@
+/**
+ * Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.
+
+maxSubarraySum([1,2,5,2,8,1,5],2) // 10
+maxSubarraySum([1,2,5,2,8,1,5],4) // 17
+maxSubarraySum([4,2,1,6],1) // 6
+maxSubarraySum([4,2,1,6,2],4) // 13
+maxSubarraySum([],4) // null
+ */
+
+// naive approach
+
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) return null;
+  let sum = -Infinity;
+  let temp = 0;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    temp = 0;
+    for (let j = i; j < i + num; j++) {
+      temp += arr[j];
+    }
+    if (temp > sum) sum = temp;
+  }
+
+  return sum;
+}
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+console.log(maxSubarraySum([4, 2, 1, 6], 1)); // 6
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)); // 13
+console.log(maxSubarraySum([], 4)); // null
+
+// sliding window approach
+
+function maxSubarraySum2(arr, num) {
+  if (num > arr.length) return null;
+  let maxSum = 0;
+  let tempSum = 0;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+}
+
+console.log(maxSubarraySum2([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubarraySum2([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+console.log(maxSubarraySum2([4, 2, 1, 6], 1)); // 6
+console.log(maxSubarraySum2([4, 2, 1, 6, 2], 4)); // 13
+console.log(maxSubarraySum2([], 4)); // null
