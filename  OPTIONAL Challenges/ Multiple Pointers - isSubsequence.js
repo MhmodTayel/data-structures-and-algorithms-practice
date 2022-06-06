6b8b1396b4e0d7ef3412b200d0b3a23f50606ceb
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2022-06-06 10:41:04 +0200

    sliding window

---
+++  OPTIONAL Challenges/ Multiple Pointers - isSubsequence.js
@@ -0,0 +1,17 @@
+/**
+ * isSubsequence(str1, str2)
+Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'acb'); // false (order matters)
+ */
+
+function isSubsequence(str1, str2) {
+  let left = 0
+  let right = 1
+  for (let i = 0; i < str2.length; i++) {
+      
+  }
+}
